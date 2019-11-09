9b81fc5921bef92c660804860babe041365a9206
Lebrra <Lebrra@users.noreply.github.com>
2019-11-09 16:50:43 -0600

    fhymj

--- PettingSimulator/Assets/Scripts/Bow.cs
+++ PettingSimulator/Assets/Scripts/Bow.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bow : MonoBehaviour
 {
     [Header("Assets")]
     public GameObject arrowPref;
+    public Text debug;
 
     [Header("Bow")]
     public float grabThreshold = 0.15F;
@@ -30,8 +32,37 @@ public class Bow : MonoBehaviour
         StartCoroutine(CreateArrow(0));
     }
 
+    private void Update()
+    {
+        if (!pullingHand || !currentArrow)
+        {
+            //debug.text = "broken";
+            return;
+        }
+        pullValue = CalculatePull(pullingHand);
+        pullValue = Mathf.Clamp(pullValue, 0, 1);
+        //set anim "Blend"
+    }
+
+    private float CalculatePull(Transform pullHand)
+    {
+        Vector3 direction = end.position - start.position;
+        float mag = direction.magnitude;
+
+        direction.Normalize();
+        Vector3 diff = pullHand.position - start.position;
+
+        return Vector3.Dot(diff, direction) / mag;
+    }
+
+    private void FireArrow()
+    {
+        currentArrow = null;
+    }
+
     private IEnumerator CreateArrow(float waitTime)
     {
+        debug.text = "New arrow made";
         yield return new WaitForSeconds(waitTime);
 
         GameObject arrowObj = Instantiate(arrowPref, spawnPoint);
@@ -42,11 +73,24 @@ public class Bow : MonoBehaviour
 
     public void Pull(Transform hand)
     {
-
+        debug.text = "Pulled arrow back";
+        float dist = Vector3.Distance(hand.position, start.position);
+        if (dist > grabThreshold)
+        {
+            debug.text = "nope";
+            return;
+        }
+        pullingHand = hand;
     }
 
     public void Release()
     {
+        debug.text = "Shot arrow";
+        if (pullValue > 0.25F) FireArrow();
+        pullingHand = null;
+        pullValue = 0;
+        // reset anim "Blend"
 
+        if(!currentArrow) StartCoroutine(CreateArrow(0.25F));
     }
 }
