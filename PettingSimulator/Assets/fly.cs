dbf0b6aaefce3ae3ea28f0fdcc4a61843cd3a26b
WesMcW <wesmcwhorter@gmail.com>
2019-11-09 13:41:07 -0600

    Moving cubes

---
+++ PettingSimulator/Assets/fly.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fly : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rb.AddForce(transform.forward * speed);
+        //transform.position = new Vector3(transform.position.x -.05f, transform.position.y, transform.position.z);
+    }
+}
---
+++ PettingSimulator/Assets/thrower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thrower : MonoBehaviour
+{
+    public Transform spawn;
+    public GameObject apple;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("shootOut", 1f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //shootOut();
+    }
+
+    void shootOut()
+    {
+        Instantiate(apple, spawn.transform.position, Quaternion.Euler(0, -90, 0));
+    }
+}
